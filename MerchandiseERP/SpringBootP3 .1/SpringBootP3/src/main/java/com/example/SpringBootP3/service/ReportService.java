72a9639944b7db7e893fb6a928ef001e25aa5a7c
Fatema Tuz Zohora <fatematuzzohora289@gmail.com>
2024-03-19 13:31:22 +0600

    up

--- MerchandiseERP/SpringBootP3 .1/SpringBootP3/src/main/java/com/example/SpringBootP3/service/ReportService.java
+++ MerchandiseERP/SpringBootP3 .1/SpringBootP3/src/main/java/com/example/SpringBootP3/service/ReportService.java
@@ -1,10 +1,18 @@
 package com.example.SpringBootP3.service;
 
+import com.example.SpringBootP3.repository.buyer.IBuyerRepo;
+import com.example.SpringBootP3.restController.BuyerRest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReportService {
 
-    
+@Autowired
+    private IBuyerRepo buyerRepo;
+
+public String exportRE
+
+
 
 }
