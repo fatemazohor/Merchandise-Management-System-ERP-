e7a2fba1073d8a38791ea198931720b95879e9e4
Fatema Tuz Zohora <fatematuzzohora289@gmail.com>
2024-03-19 17:47:51 +0600

    jasper report

--- MerchandiseERP/SpringBootP3 .1/SpringBootP3/src/main/java/com/example/SpringBootP3/restController/BuyerRest.java
+++ MerchandiseERP/SpringBootP3 .1/SpringBootP3/src/main/java/com/example/SpringBootP3/restController/BuyerRest.java
@@ -2,11 +2,15 @@ package com.example.SpringBootP3.restController;
 
 import com.example.SpringBootP3.model.buyer.Buyers;
 import com.example.SpringBootP3.repository.buyer.IBuyerRepo;
+import com.example.SpringBootP3.service.ReportService;
+import net.sf.jasperreports.engine.JRException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.FileNotFoundException;
 import java.util.List;
 
 @RestController
@@ -15,9 +19,16 @@ public class BuyerRest {
 
     @Autowired
     private IBuyerRepo buyerRepo;
+    @Autowired
+    private ReportService reportService;
 
     @GetMapping("/buyer_list_rest")
     public List<Buyers> getBuyers(){
         return buyerRepo.findAll();
+    }
+
+    @GetMapping("/report/{format}")
+    public String generateReport(@PathVariable String format) throws JRException, FileNotFoundException {
+        return reportService.exportReport(format);
     }
 }
--- MerchandiseERP/SpringBootP3 .1/SpringBootP3/src/main/java/com/example/SpringBootP3/service/ReportService.java
+++ MerchandiseERP/SpringBootP3 .1/SpringBootP3/src/main/java/com/example/SpringBootP3/service/ReportService.java
@@ -1,9 +1,19 @@
 package com.example.SpringBootP3.service;
 
+import com.example.SpringBootP3.model.buyer.Buyers;
 import com.example.SpringBootP3.repository.buyer.IBuyerRepo;
 import com.example.SpringBootP3.restController.BuyerRest;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class ReportService {
@@ -11,7 +21,29 @@ public class ReportService {
 @Autowired
     private IBuyerRepo buyerRepo;
 
-public String exportRE
+public String exportReport(String reportFormat) throws FileNotFoundException, JRException {
+    String path="F:\\New folder\\BOOK\\New folder\\github\\Merchandise-Management-System-ERP-\\reports";
+    // data list
+    List<Buyers> buyers = buyerRepo.findAll();
+    //load file
+    File file = ResourceUtils.getFile("F:\\New folder\\BOOK\\New folder\\github\\Merchandise-Management-System-ERP-\\MerchandiseERP\\SpringBootP3 .1\\SpringBootP3\\src\\main\\resources\\reports\\buyers.jrxml");
+    //compile file for jasper
+    JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
+    JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(buyers);
+    Map<String,Object> para = new HashMap<>();
+    para.put("cratedBy","Fatema Tuz Zohora");
+    JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport,para,dataSource);
+    if(reportFormat.equalsIgnoreCase("html")){
+        JasperExportManager.exportReportToHtmlFile(jasperPrint,path+"\\buyersList.html");
+    }
+    if(reportFormat.equalsIgnoreCase("pdf")){
+        JasperExportManager.exportReportToPdfFile(jasperPrint,path+"\\buyersList.pdf");
+    }
+    return "report generated in path:"+path;
+
+
+
+}
 
 
 
